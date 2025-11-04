c27f72ea86c00d0de6bc182563674b19babcce4a
Marshmelloows <Nightdarkplane@gmail.com>
2025-11-05 11:51:46 +1300

    Add files via upload

---
+++ jhons class/ASSIGNMENT THREE ITPF5.110 Programming Fundamentals/l3/Cinima class.py
@@ -0,0 +1,42 @@
+#Cinima class
+
+import Movie
+import Bookings
+import Custiomer
+
+#-------------------<Class~~Cinema>------------#
+
+class Cinema:
+    #Constructor
+    def __init__(self, movies, bookings, custiomer):
+        self._movies = movies #list of movies
+        self._bookings: list = bookings #list of bookings
+        self._Custiomer: list = custiomer #list of custiomers
+
+
+
+#--------------<ADD() MOVIES>-----------#
+#    def add(self):
+       # if self._movies
+#-------------<Get~Movie~Names()>---------#
+
+#-------------<Remove>-----------#
+    def remove(self, movie: str):
+        if not movie:#Checks for valid movie
+            raise ValueError("must give the name of the movie")
+        for i in range(len(self._movies)):
+            if self._movies[i].code == movie:
+                del self._movies[i]
+            break
+    
+        self._movies
+
+#-------------<Find~Movie~Items>-------------#
+
+#-------------<Find~Rated~Movie~Names>------------#
+
+#-------------<Get~Total~Minutes>-------------#
+
+
+        
+
